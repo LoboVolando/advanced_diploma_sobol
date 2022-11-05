949215930b31153ef64c592b55583c4d598beeff
svv <0507033@211.ru>
2022-11-05 23:43:38 +0700

    настраиваем ci-cd

--- fabfile.py
+++ fabfile.py
@@ -32,5 +32,6 @@ def test(ctx):
             c.run("git pull origin dev --rebase")
             c.run("docker-compose -f docker-compose.test.yml stop postgres")
             c.run("docker-compose -f docker-compose.test.yml rm postgres")
-            c.run("python3 -m venv venv && source ./venv/bin/activate")
-            c.run('python3 -c "import sys; print(sys.executable)"')
+            c.run("python3 -m venv venv")
+            c.run("source ./venv/bin/activate")
+            c.run('python -c "import sys; print(sys.executable)"')
