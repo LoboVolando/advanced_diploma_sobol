1ba6f34782ade0a5ae9df159050edd7c5b501141
svv <0507033@211.ru>
2022-11-05 22:04:27 +0700

    настраиваем ci-cd

--- fabfile.py
+++ fabfile.py
@@ -11,11 +11,11 @@ def deploy(ctx):
             connect_kwargs={"key_filename": os.getenv('DEV_PRIVATE_KEY')}
     ) as c:
         with c.cd("/home/svv/projects/py/advanced_diploma"):
-            c.run("docker-compose -f docker-compose.test.yml down")
-            c.run("docker-compose -f docker-compose.deploy.yml down")
             c.run("git stash clear")
             c.run("git stash -m 'to drop'")
             c.run("git pull origin dev --rebase")
+            c.run("docker-compose -f docker-compose.test.yml down")
+            c.run("docker-compose -f docker-compose.deploy.yml down")
             c.run("docker-compose -f docker-compose.deploy.yml up --build -d backend frontend postgres pgadmin")
 
 @task
@@ -27,9 +27,9 @@ def test(ctx):
     ) as c:
         with c.cd("/home/svv/projects/py/advanced_diploma"):
             c.run("ls")
-            c.run("docker-compose -f docker-compose.test.yml down")
-            c.run("docker-compose -f docker-compose.deploy.yml down")
             c.run("git stash clear")
             c.run("git stash -m 'to drop'")
             c.run("git pull origin dev --rebase")
+            c.run("docker-compose -f docker-compose.test.yml down")
+            c.run("docker-compose -f docker-compose.deploy.yml down")
             c.run("docker-compose -f docker-compose.test.yml up --build -d postgres backend")
