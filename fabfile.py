fe226edc79a134af9aadb3a3138f78f91757aa1b
svv <0507033@211.ru>
2022-11-06 21:24:28 +0700

    test worked

--- fabfile.py
+++ fabfile.py
@@ -14,25 +14,6 @@ def deploy(ctx):
             c.run("git stash clear")
             c.run("git stash -m 'to drop'")
             c.run("git pull origin dev --rebase")
-            c.run("docker-compose -f docker-compose.test.yml down")
-            c.run("docker-compose -f docker-compose.deploy.yml down")
-            c.run("docker-compose -f docker-compose.deploy.yml up --build -d backend frontend postgres pgadmin")
-
-@task
-def test(ctx):
-    with Connection(
-            host=os.getenv('DEV_HOST'),
-            user=os.getenv('DEV_USER'),
-            connect_kwargs={"key_filename": os.getenv('DEV_PRIVATE_KEY')}
-    ) as c:
-        with c.cd("/home/svv/projects/py/advanced_diploma"):
-            c.run("ls")
-            c.run("git stash clear")
-            c.run("git stash -m 'to drop'")
-            c.run("git pull origin dev --rebase")
-            # c.run("")
-            # c.run("")
-            c.run("python3 -m venv new_venv")
-            c.run("source ./new_venv/bin/activate")
-            c.run('python -V')
-            c.run("echo activated")
+            c.run("docker-compose -f docker-compose.test.yml stop")
+            c.run("docker-compose -f docker-compose.deploy.yml stop")
+            c.run("docker-compose -f docker-compose.deploy.yml up --build -d fluentd backend frontend postgres pgadmin")
