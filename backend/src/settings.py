12c3b0aa45ce476052f3c2ce2d311c71a4484c8f
svv <0507033@211.ru>
2022-11-06 11:51:00 +0700

    настраиваем ci-cd

--- backend/src/settings.py
+++ backend/src/settings.py
@@ -13,20 +13,11 @@ class Settings(BaseSettings):
     web_db: str = "postgres"
     redis_host: str = "localhost"
     redis_port: str = 5479
-    docker_media_root: str = "/home/svv/projects/data/diploma/static/media"
+    docker_media_root: str = "/home/svv/data/test-diploma/static/media"
     media_url: str = "/static/media"
-
-    # db_url = ""
-    # async_db_url = ""
-    # connect_args = {"check_same_thread": False}
-    # jwt_secret: str
-    # jwt_algorithm: str = "HS256"
-    # jwt_expiration: int = 3600
 
 
 if os.path.exists("./.env"):
     settings = Settings(_env_file="./.env", _env_file_encoding="UTF-8")
-    print(settings.postgres_port)
 else:
     settings = Settings()
-    print("create")
